206eb5f42e54db1d1a5d901ec9bf944b22b564fb
clbrem <cbremer4@gmail.com>
2023-07-23 16:55:09 -0500

    working on database.js

---
+++ src/Client/wwwroot/js/Database.js
@@ -0,0 +1,26 @@
+﻿const table = "Input"
+const version = 1
+Database = {
+    write: writeData
+}
+function writeData(data) {
+    return new Promise((resolve,reject) => {
+        const write= (db) => {
+            const trans = db.transaction(table, "readwrite")
+            const objectStore = trans.objectStore(table)
+            trans.onerror = reject
+            const req = objectStore.add(data)
+            req.onsuccess = e => resolve(e.target.result)
+        }
+        const req = indexedDB.open(table, version)
+        req.onerror = reject
+        req.onupgradedneeded = e => {
+            const db = e.target.result
+            const objectStore = 
+                db.createObjectStore(table, {autoIncrement: true})
+            objectStore.createIndex(table, "session", {unique: false})
+            objectStore.transaction.oncomplete = () => write(db)
+        }
+        req.onsuccess = e => write(e.target.result)
+    })
+}
